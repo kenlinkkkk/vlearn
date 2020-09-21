daa6d78696224490fc66c5a8d5360ebc16537ab5
thanh <tungnt15ptit@gmail.com>
2020-09-21 11:25:36 +0700

    fix

--- resources/views/admin/package/add.blade.php
+++ resources/views/admin/package/add.blade.php
@@ -40,7 +40,7 @@
                         <div class="form-group">
                             <label>Ảnh nền <span class="text-danger">(*)</span></label>
                             <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="customFile" name="picture" required>
+                                <input type="file" class="custom-file-input" id="customFile" name="picture">
                                 <label class="custom-file-label" for="customFile">Choose file</label>
                             </div>
                         </div>

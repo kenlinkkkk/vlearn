85d61b153af3c6bfb35e41b8e5f590d286a4bd67
Tung <tungnt@vano.vn>
2021-03-01 17:43:56 +0700

    fix query search

--- app/Http/Controllers/Admin/LessonController.php
+++ app/Http/Controllers/Admin/LessonController.php
@@ -23,7 +23,7 @@ class LessonController extends Controller
     {
         if (!empty($request->get('q'))) {
             $query = '%'.url_slug($request->get('q')).'%';
-            $lessons = Lesson::query()->where('status', '=', 1)->where('slug', 'like', $query)->with('withPackage')->paginate(10);
+            $lessons = Lesson::query()->where('status', '=', 1)->where('slug', 'like', $query)->with('withPackage')->paginate(10)->appends('q');
         } else {
             $lessons = Lesson::query()->where('status', '=', 1)->with('withPackage')->paginate(10);
         }
